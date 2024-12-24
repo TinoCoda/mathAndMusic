b6d996268444c987be664f3f6f9d5498f83cce0c
Destin B. Moanda <moanda@live.fr>
2024-12-24 12:49:36 +0100

    define custom track styles

--- track.js
+++ track.js
@@ -1,18 +1,77 @@
+const trackStyle=Object.freeze(
+   {
+      CIRCLE: 0,
+      OVAL: 1,
+      HYPERBOLE: 2,
+   }
+)
 class Track {
     constructor(center, radius, hue) {
        this.center = center;
        this.radius = radius;
        this.hue = hue;
        this.period = Math.PI;
+       this.style=trackStyle.CIRCLE;
     }
  
-    getPosition(offset) {
+    getPositionOld(offset) {
        return {
-          x: this.center.x + Math.cos(offset) * this.radius,
-          y: this.center.y - Math.abs(Math.sin(offset)) * this.radius,
+          x: this.center.x + this.radius*0.9*Math.cos(offset), //* this.radius,
+          y: this.center.y - Math.abs(Math.sin(offset)) * 0.7*this.radius,
           round: Math.floor(offset / this.period),
           progress: (offset % this.period) / this.period
        };
+    }
+
+    setTrackStyle(i)
+    {
+      const possibleStyles=Object.values(trackStyle);
+      const selector= i % 5;
+      if(possibleStyles.includes(selector)){
+         this.style=selector;
+      }
+      else{
+         this.style=trackStyle.CIRCLE;
+      }
+
+      
+    }
+
+
+    getPosition(offset) {
+      let x=0;
+      let y=0;
+      let round=0;
+      let progress=0;
+      switch(this.style)
+      {
+         case trackStyle.CIRCLE:
+            x = this.center.x + this.radius*Math.cos(offset);
+            y = this.center.y - Math.abs(Math.sin(offset)) *this.radius;
+            round = Math.floor(offset / this.period);
+            progress = (offset % this.period) / this.period;
+            break;
+         case trackStyle.OVAL:
+            x = this.center.x + this.radius*0.9*Math.cos(offset);
+            y = this.center.y - Math.abs(Math.sin(offset)) * 0.5*this.radius;
+            round = Math.floor(offset / this.period);
+            progress = (offset % this.period) / this.period;
+            break;
+         case trackStyle.HYPERBOLE:
+            x = this.center.x + this.radius*0.9/Math.cos(offset);
+            y = this.center.y - Math.abs(Math.tan(offset)) * 0.7*this.radius;
+            round = Math.floor(offset / this.period);
+            progress = (offset % this.period) / this.period;
+            break;
+      }
+      return {
+         x: x,
+         y: y,
+         round: round,
+         progress: progress
+      };
+
+   
     }
  
     draw(ctx) {
